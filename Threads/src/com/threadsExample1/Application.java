f9424f2803bb97d5e3f3f92853388fc370549658
exquilla98 <dinukamalshan14@gmail.com>
2022-09-07 17:03:06 +0530

    My changes

--- Threads/src/com/threadsExample1/Application.java
+++ Threads/src/com/threadsExample1/Application.java
@@ -8,6 +8,7 @@ public class Application {
 		Thread thread=new Thread(printer);
 		//thread.setPriority(1);
 		thread.start();
+		thread.interrupt();
 		//thread.join();
 		for (int i = 0; i < 100; i++) {
 			System.out.println("This is from main thread"+i);
