0d586bc046acd585bda2727c60ab04bd4341fa52
exquilla98 <dinukamalshan14@gmail.com>
2022-09-09 17:12:50 +0530

    MS with Kafka change

--- Kafka-MS-example/src/com/exquilla/kafkaExample/sampleConsumer.java
+++ Kafka-MS-example/src/com/exquilla/kafkaExample/sampleConsumer.java
@@ -1,6 +1,10 @@
 package com.exquilla.kafkaExample;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Properties;
 
 class sampleConsumer {
@@ -11,8 +15,29 @@ class sampleConsumer {
 	       props.put("bootstrap.servers", "localhost:9092");
 	       props.put("group.id","test-group");
 	       
+	       props.put("enable.auto.commit","true");
 	       
-	       kafkaConsumer<String,String>consumer = new kafkaConsumer<>
+	       props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+	       props.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+	       
+	       KafkaConsumer<String,String>consumer = new KafkaConsumer<String,String>(props);
+	       
+	       consumer.subscribe(Arrays.asList("source-topic"));
+	       
+	       
+	       while (true)) {
+	    	ConsumerRecords<String,String>records=consumer.poll(100);
+	    	
+	    	for (ConsumerRecord<String, String>record:records) {
+	    		System.out.printf("offset=%d, key=%s,value= %s\n", record.offset(),record.key(),record.value();
+				
+			}
+	    	
+	    	   
+			
+			
+		}
+	       
 	       
 
 	}
